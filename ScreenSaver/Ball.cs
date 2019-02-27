f17c5d35d4dd8027064962cf47b79905c5fbddf8
Andrew Henderson <Andrhend062@ed.amdsb.ca>
2019-02-26 23:38:39 -0500

    Add project files.

---
+++ ScreenSaver/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace ScreenSaver
+{
+    class Ball
+    {
+        public SolidBrush brush;
+        Random rand = new Random();
+        public int x, y, xS, yS, size;
+        public bool right, down;
+        public Ball(int width, int height, Color colour)
+        {
+
+            x = rand.Next(width);
+            y = rand.Next(height);
+            xS = rand.Next(10);
+            yS = rand.Next(10);
+            size = rand.Next(10);
+            right = Convert.ToBoolean(rand.Next(0, 2));
+            down = Convert.ToBoolean(rand.Next(0, 2));
+            brush = new SolidBrush(colour);
+        }
+    }
+}
---
+++ ScreenSaver/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class Form1 : Form
+    {
+        List<Ball> balls = new List<Ball>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            OnStart();
+            DoubleBuffered = true;
+
+        }
+        public void OnStart()
+        {
+            balls.Add(new Ball(Width,Height,Color.Blue));
+        }
+
+        private void gameTime_Tick(object sender, EventArgs e)
+        {
+
+
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Ball b in balls)
+            {
+                e.Graphics.FillEllipse(b.brush, b.x, b.y, b.size, b.size);
+            }
+        }
+    }
+   
+}
