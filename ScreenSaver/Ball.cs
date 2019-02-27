74eff1edb54a9e7a6bd5c4aa8987058e20541d5e
Andrew Henderson <andrhend062@ed.amdsb.ca>
2019-02-27 10:03:19 -0500

    wendsday

--- ScreenSaver/Ball.cs
+++ ScreenSaver/Ball.cs
@@ -4,25 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Windows.Forms;
+
 namespace ScreenSaver
 {
     class Ball
     {
         public SolidBrush brush;
-        Random rand = new Random();
+        
         public int x, y, xS, yS, size;
         public bool right, down;
+        
+        
         public Ball(int width, int height, Color colour)
         {
 
-            x = rand.Next(width);
-            y = rand.Next(height);
-            xS = rand.Next(10);
-            yS = rand.Next(10);
-            size = rand.Next(10);
-            right = Convert.ToBoolean(rand.Next(0, 2));
-            down = Convert.ToBoolean(rand.Next(0, 2));
+            x = Form1.rand.Next(width);
+            y = Form1.rand.Next(height);
+            xS = Form1.rand.Next(6);
+            yS = Form1.rand.Next(6);
+            size = Form1.rand.Next(30,50);
+            right = Convert.ToBoolean(Form1.rand.Next(0, 2));
+            down = Convert.ToBoolean(Form1.rand.Next(0, 2));
             brush = new SolidBrush(colour);
+
+            
+        }
+        public void Move()
+        {
+            if (right)
+            {
+                x = x + xS;
+            }
+            else
+            {
+                x = x - xS;
+            }
+
+            if (down)
+            {
+                y = y + yS;
+            }
+            else
+            {
+                y = y - yS;
+            }
+        }
+
+        public Boolean Collision(Ball b)
+        {
+
+            Rectangle rect = new Rectangle(x, y, size, size);
+            Rectangle ballRect = new Rectangle(b.x, b.y, b.size, b.size);
+            return rect.IntersectsWith(ballRect);
+            
+            
         }
     }
 }
--- ScreenSaver/Form1.cs
+++ ScreenSaver/Form1.cs
@@ -13,24 +13,40 @@ namespace ScreenSaver
     public partial class Form1 : Form
     {
         List<Ball> balls = new List<Ball>();
-        
+        public static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
             OnStart();
             DoubleBuffered = true;
 
-        }/// <summary>
-        /// 
-        /// </summary>
+        }
         public void OnStart()
         {
             balls.Add(new Ball(Width,Height,Color.Blue));
+            gameTime.Enabled = true;
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            balls.Add(new Ball(Width, Height, Color.Red));
         }
 
         private void gameTime_Tick(object sender, EventArgs e)
         {
+            foreach(Ball b in balls)
+            {
 
+                b.Move();
+               // b.Collision(balls);
+                if (b.x <= 0||b.x+b.size >=Width)
+                {
+                    b.right = !b.right;
+                }
+                if (b.y <= 0 || b.y+b.size >= Height)
+                {
+                    b.down = !b.down;
+                }
+            }
 
 
             Refresh();
@@ -43,6 +59,8 @@ namespace ScreenSaver
                 e.Graphics.FillEllipse(b.brush, b.x, b.y, b.size, b.size);
             }
         }
+
+       
     }
    
 }
